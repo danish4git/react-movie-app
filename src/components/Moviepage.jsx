0e121859bace28c53a1fb5975921f4d57338a7b6
DANISH AHMED <danish4eng@gmail.com>
2022-05-01 18:29:17 +0530

    added filters

--- src/components/Moviepage.jsx
+++ src/components/Moviepage.jsx
@@ -4,17 +4,31 @@ import { Link } from "react-router-dom";
 import "./moviepage.css";
 
 const Moviepage = () => {
-  const [input, setinput] = useState("kick");
+  const [input, setinput] = useState("spider");
   const [getmovies, setgetmovies] = useState([]);
+  const [movietype, setmovietype] = useState(null);
   const getdata = () => {
-    axios
-      .get(
-        `https://api.themoviedb.org/3/search/movie?api_key=9d1ba1d73acfde590055a0c392c675b2&language=en-US&page=1&include_adult=false&query=${input}`
-      )
-      .then((res) => {
-        setgetmovies(res.data.results);
-        console.log(res.data.results);
-      });
+    //////////https://api.themoviedb.org/3/movie/top_rated?api_key=9d1ba1d73acfde590055a0c392c675b2&language=en-US&page=1
+
+    if (movietype) {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/${movietype}?api_key=9d1ba1d73acfde590055a0c392c675b2&language=en-US&page=1`
+        )
+        .then((res) => {
+          setgetmovies(res.data.results);
+          console.log(`movie type :${movietype}`);
+        });
+    } else {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=9d1ba1d73acfde590055a0c392c675b2&language=en-US&page=1&include_adult=false&query=${input}`
+        )
+        .then((res) => {
+          setgetmovies(res.data.results);
+          console.log(res.data.results);
+        });
+    }
   };
 
   let id;
@@ -30,7 +44,7 @@ const Moviepage = () => {
   useEffect(() => {
     getdata();
     // console.log(`movies ${movies}`);
-  }, [input]);
+  }, [input, movietype]);
 
   return (
     <div>
@@ -40,9 +54,33 @@ const Moviepage = () => {
           type="text"
           placeholder="Search Movie"
           onChange={(e) => {
+            setmovietype(null);
             debouncing(e);
           }}
         />
+      </div>
+      <div className="filterbtn">
+        <button
+          onClick={() => {
+            setmovietype("popular");
+          }}
+        >
+          Popular
+        </button>
+        <button
+          onClick={() => {
+            setmovietype("upcoming");
+          }}
+        >
+          Upcoming
+        </button>
+        <button
+          onClick={() => {
+            setmovietype("top_rated");
+          }}
+        >
+          Top Rated
+        </button>
       </div>
 
       <div className="showmovie">
@@ -66,7 +104,7 @@ const Moviepage = () => {
             );
           })
         ) : (
-          <div>no data</div>
+          <div>Loading...</div>
         )}
       </div>
     </div>
